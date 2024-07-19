6d2f5b11737b9c020fda3fe2e7777b7a04c006b3
Demigorgan123 <rbanswal123@gmail.com>
2024-07-19 17:47:10 +0530

    completed the design of Hero page and added handleClick funcionality on Refer Now button to open a form inside a modal.

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,82 @@
+import { Dispatch, SetStateAction } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type modalProps = {
+    toggleModal: Dispatch<SetStateAction<boolean>>
+}
+type Inputs = {
+    referTo: string,
+    referFrom: string,
+    referralCode: string
+}
+function Modal(props: modalProps) {
+    const handleClose = () => {
+        props.toggleModal(false)
+    }
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <div className="max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-md">
+                <h2 className="text-3xl font-bold mb-8 text-center text-[#1A73E8]">Referral Form</h2>
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-4">
+                    <div>
+                        <label className="block text-gray-700 mb-2">Refer From</label>
+                        <input {...register("referFrom", { 
+                            required: "This is required",
+                            pattern: { 
+                                value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                                message: "Enter valid email id" } 
+                            })}
+                            type="email"
+                            className="block w-full mt-1 rounded-lg border-gray-300 border-[1px] outline-none px-5 py-2"
+                            placeholder="referee@example.com" />
+                        <p className="text-red-600">{errors.referFrom?.message}</p>
+                    </div>
+                    <div>
+                        <label className="block text-gray-700 mb-2">Refer To</label>
+                        <input {...register("referTo", { 
+                            required: "This is required",
+                            pattern:{
+                                value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                                message: "Enter valid email id"
+                            }
+                        })}
+                            type="email"
+                            className="block w-full mt-1 rounded-lg border-gray-300 border-[1px] outline-none px-5 py-2"
+                            placeholder="referral@example.com" />
+                        <p className="text-red-600">{errors.referTo?.message}</p>
+                    </div>
+                    <div>
+                        <label className="block text-gray-700 mb-2">Referral Code</label>
+                        <input {...register("referralCode", { 
+                            required: "This is required", 
+                            minLength: { 
+                                value: 8, 
+                                message: "Min length is 8" 
+                            }, 
+                            pattern: { 
+                                value: /^[A-Z0-9]{8}$/,
+                                message: "Enter valid code" 
+                                }, 
+                            maxLength: { 
+                                value: 8, 
+                                message: "Max length is 8"
+                            } 
+                        })}
+                            type="text"
+                            className="block w-full mt-1 rounded-lg border-gray-300 border-[1px] outline-none px-5 py-2"
+                            placeholder="REF12345" />
+                        <p className="text-red-600">{errors.referralCode?.message}</p>
+                    </div>
+                    <div>
+                        <input type="submit"
+                            className="w-full bg-[#1A73E8] text-white py-3 rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 cursor-pointer" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -1,42 +1,53 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import banner from "../images/banner.png";
 import money from "../images/money.png";
+import Modal from "../components/Modal";
 
 function Hero() {
+    const [isClicked, setIsClicked] = useState<boolean>(false);
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    }
     return (
         <>
-            <Header />
-            <Navbar />
-            <div>
-                <div className="bg-[#EEF5FF] m-auto w-[800px] my-10 flex items-center rounded-2xl shadow-[1px_2px_30px_rgba(0,0,0,0.2)] px-8 gap-8 relative">
-                    <div className="absolute w-20 top-0 left-0">
-                        <img src={money} alt="money" />
-                    </div>
-                    <div className="absolute w-20 top-0 right-0 rotate-90">
-                        <img src={money} alt="money" />
-                    </div>
-                    <div className="flex flex-col w-[35%] mt-8">
-                        <p className="text-5xl font-bold">Let's Learn & Earn</p>
-                        <p className="text-2xl">Get a chance to win up-to <span className="text-3xl text-[#1A73E8] font-semibold">Rs. 15,000</span></p>
-                        <button className="bg-[#1A73E8] text-white rounded-md py-2 px-4 w-fit mt-16">Refer Now</button>
-                    </div>
-                    <div className="w-[65%]">
-                        <div className="h-full w-full relative">
-                            <img src={banner} alt="banner" className="w-full h-full scale-[1.4]" />
-                            <div className="absolute w-20 top-4 left-12 rotate-180">
-                                <img src={money} alt="money" />
-                            </div>                    
-                            <div className="absolute w-20 bottom-4 left-0 rotate-90">
-                                <img src={money} alt="money" />
-                            </div>                    
-                            <div className="absolute w-20 top-48 right-0">
-                                <img src={money} alt="money" />
+            {
+                isClicked ? <Modal toggleModal={setIsClicked} /> :
+                    <div>
+                        <Header />
+                        <Navbar />
+                        <div>
+                            <div className="bg-[#EEF5FF] m-auto w-[800px] my-10 flex items-center rounded-2xl shadow-[1px_2px_30px_rgba(0,0,0,0.2)] px-8 gap-8 relative">
+                                <div className="absolute w-20 top-0 left-0">
+                                    <img src={money} alt="money" />
+                                </div>
+                                <div className="absolute w-20 top-0 right-0 rotate-90">
+                                    <img src={money} alt="money" />
+                                </div>
+                                <div className="flex flex-col w-[35%] mt-8">
+                                    <p className="text-5xl font-bold">Let's Learn & Earn</p>
+                                    <p className="text-2xl">Get a chance to win up-to <span className="text-3xl text-[#1A73E8] font-semibold">Rs. 15,000</span></p>
+                                    <button type="button" onClick={handleClick} className="bg-[#1A73E8] text-white rounded-md py-2 px-4 w-fit mt-16">Refer Now</button>
+                                </div>
+                                <div className="w-[65%]">
+                                    <div className="h-full w-full relative">
+                                        <img src={banner} alt="banner" className="w-full h-full scale-[1.4]" />
+                                        <div className="absolute w-20 top-4 left-12 rotate-180">
+                                            <img src={money} alt="money" />
+                                        </div>
+                                        <div className="absolute w-20 bottom-4 left-0 rotate-90">
+                                            <img src={money} alt="money" />
+                                        </div>
+                                        <div className="absolute w-20 top-48 right-0">
+                                            <img src={money} alt="money" />
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+            }
         </>
     );
 }
