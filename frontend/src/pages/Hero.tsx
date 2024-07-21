56a5d8077d5dd1b9a39c20dc1f7800f071d6959a
Demigorgan123 <rbanswal123@gmail.com>
2024-07-22 00:57:40 +0530

    bug fix

--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Modal from "../components/Modal";
-import Banner from "../components/Banner";
+import Banner from "../components/Banner"
 
 function Hero() {
     const [isClicked, setIsClicked] = useState<boolean>(false);
